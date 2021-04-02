a12d85fef1fdec163b62274b91c648a0c2f80778
Felix <felix.dere8@gmail.com>
2021-04-02 12:04:28 +0200

    Validation Suppression de matelas

--- supress.php
+++ supress.php
@@ -4,13 +4,14 @@ $db = new PDO('mysql:host=localhost;dbname=litterie3000', "root", "");
 if(isset($_GET["id"])){
     $id = $_GET["id"];
 
-    $queryOne = $db->prepare("DELETE FROM lit WHERE id = :id");
-    $queryOne->bindParam(":id", $id, PDO::PARAM_INT);
-    $queryOne->execute();
     $queryTwo = $db->prepare("DELETE FROM lit_size WHERE lit_id = :id");
     $queryTwo->bindParam(":id", $id, PDO::PARAM_INT);
+    $queryTwo->execute();
 
-        if($queryTwo->execute()){
+    $queryOne = $db->prepare("DELETE FROM lit WHERE id = :id");
+    $queryOne->bindParam(":id", $id, PDO::PARAM_INT);
+
+        if($queryOne->execute()){
             header("Location: index.php");
         }
 }
